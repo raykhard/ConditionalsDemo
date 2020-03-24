33f4c5d2715ce6c551d8190d0c82fda15d691abc
raykhard <61735357+raykhard@users.noreply.github.com>
2020-03-24 09:05:19 +0300

    commented

--- demo.js
+++ demo.js
@@ -5,6 +5,8 @@ var msecBtnNowAnd1970 = Date.now();
 var ageInMsec = msecBtnNowAnd1970-msecBtndateOfBirthAnd1970;
 //use Date.parse and Date.now to calculate the age in msec.
 
+
+//converting milliseconds to years.
 var milliseconds = ageInMsec;
 var second = 1000;
 var minute = second*60;
@@ -13,17 +15,19 @@ var day = hour*24;
 var month = day*30;
 var year = month*12;
 
+
 var years = Math.round(milliseconds/year);
 
-function result(val){
+function result(x){
     var ageOutcome = years;
-    if (ageOutcome<18){
-        console.log("minor");
-    }else if (ageOutcome<=18 && ageOutcome<36){
-        console.log ("youth");
-    }else
-    console.log ("elder");
+        if (ageOutcome<18){
+            console.log("minor");
+        }else if (ageOutcome<=18 && ageOutcome<36){
+            console.log ("youth");
+        }else {
+        console.log ("elder");
+        }
     document.getElementById("demo").innerHTML = ageOutcome;
 }
-
+yearsCon=result;
 window.onload = result;
